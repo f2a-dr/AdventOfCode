33869e12b14060e957eb78fe77567e30de8e3d40
Francesco De Roma <francesco.deroma92@gmail.com>
2023-12-16 01:02:37 +0100

    Solved part 1 of day 4 of 2023 calendar

--- AoC_2023/day_4/firstSolution.py
+++ AoC_2023/day_4/firstSolution.py
@@ -1,4 +1,28 @@
 # Advent of Code 2023
 # Day 4: Scratchcards -- Part 1
 
+filename = "puzzle_input.txt"
+
+points = 0
+
+with open(filename) as f:
+    for line in f:
+        line = line.strip()
+        line = line.split(':')[1]
+        winningNumbers = line.split("|")[0].split()
+        elfNumbers = line.split("|")[1].split()
+
+
+        wins = 0
+        for num in elfNumbers:
+            if num in winningNumbers:
+                # print("{} is winning".format(num))
+                wins += 1
+
+        if wins > 0:
+            points += 2**(wins - 1)
+
+print(points)
+        
+
 
