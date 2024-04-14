55df4e272abd9749cc8a8153f762c3f69d36213d
Francesco De Roma <francesco.deroma92@gmail.com>
2024-04-15 01:17:02 +0200

    updates on day 5, test works, puzzle has too high numbers

--- AoC_2023/day_5/firstSolution.py
+++ AoC_2023/day_5/firstSolution.py
@@ -1,7 +1,9 @@
 # Advent of Code 2023
 # Day 5: If You Give A Seed A Fertilizer
 
-filename = "test_input.txt"
+import numpy as np
+
+filename = "puzzle_input.txt"
 
 def appender(counter, inputList, outputList):
     counter += 1
@@ -11,6 +13,21 @@ def appender(counter, inputList, outputList):
         if counter == (len(inputList)-1):
             break
         counter += 1
+
+def mapper(inputList):
+    highMax = 600000000
+
+    outputList = np.linspace(0, highMax, highMax+1, dtype=int)
+    # print(outputList)
+    for _ in range(len(inputList)):
+        newVal = inputList[_][0]
+        for __ in range(inputList[_][1], inputList[_][1]+inputList[_][2]):
+            outputList[__] = int(newVal)
+            newVal += 1
+
+    return outputList
+
+
 
 seeds = []
 seedToSoil = []
@@ -25,7 +42,6 @@ with open(filename) as f:
     lines = f.readlines()
     for _ in range(len(lines)):
         if lines[_].startswith("seeds:"):
-            print(lines[_])
             line = lines[_].strip().split(":")
             line = line[1].strip().split(" ")
             for __ in range(len(line)):
@@ -46,11 +62,24 @@ with open(filename) as f:
             appender(_, lines, humidityToLocation)
 
 
-print(seeds)
-print(seedToSoil)
-print(soilToFertilizer)
-print(fertilizerToWater)
-print(waterToLight)
-print(lightToTemperature)
-print(temperatureToHumidity)
-print(humidityToLocation)
+seedToSoilMap = mapper(seedToSoil)
+soilToFertilizerMap = mapper(soilToFertilizer)
+fertilizerToWaterMap = mapper(fertilizerToWater)
+waterToLightMap = mapper(waterToLight)
+lightToTemperatureMap = mapper(lightToTemperature)
+temperatureToHumidityMap = mapper(temperatureToHumidity)
+humidityToLocationMap = mapper(humidityToLocation)
+
+
+# print(seedToSoilMap)
+# print(soilToFertilizerMap)
+# print(fertilizerToWaterMap)
+# print(waterToLightMap)
+# print(lightToTemperatureMap)
+# print(temperatureToHumidityMap)
+# print(humidityToLocationMap)
+
+location = []
+for _ in seeds:
+    location.append(humidityToLocationMap[temperatureToHumidityMap[lightToTemperatureMap[waterToLightMap[fertilizerToWaterMap[soilToFertilizerMap[seedToSoilMap[_]]]]]]])
+print(min(location))
